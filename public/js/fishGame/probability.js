4fb9213a2c8e9ed92ff56660033a35b9f08b0503
JumesP <118614403+JumesP@users.noreply.github.com>
2024-07-08 07:04:36 +1000

    init

---
+++ public/js/fishGame/probability.js
@@ -0,0 +1,29 @@
+// import fish_chances from '../gameconfig.json';
+
+const fish_chances = {
+	"cod": 60,
+	"salmon": 20,
+	"goldfish" : 10,
+	"trash": 6,
+	"treasure": 2,
+	"jellyfish": 1.89,
+	"shark": 0.1,
+	"whale": 0.01
+}
+
+
+console.log()
+
+function getRandomFish() {
+	let randomNum = Math.floor(Math.random() * 10000) / 100; // Random number between 0 and 100
+	console.log(randomNum);
+	for (const fish in fish_chances) {
+		randomNum -= fish_chances[fish];
+		if (randomNum <= 0) {
+			console.log(fish);
+			return fish;
+		}
+	}
+}
+
+export default getRandomFish;
---
+++ public/js/index.js
@@ -0,0 +1,36 @@
+import Fish from "./classes/fish.js"
+import getRandomFish from "./fishGame/probability.js"
+
+const fish = getRandomFish()
+const fish2 = getRandomFish()
+const fish3 = getRandomFish()
+
+const fishes_in_the_fishtank = []
+
+
+const salmon = new Fish(10,5,20)
+
+
+const fishHealth = document.getElementById("fishHealth")
+fishHealth.textContent = salmon.catch()
+
+const fishtank = document.getElementById("fishtank")
+
+const fishButton = document.getElementById("fishButton")
+fishButton.addEventListener("click", function(){
+	const fish = getRandomFish()
+	fishes_in_the_fishtank.push(fish)
+	const para = document.createElement("p");
+	const node = document.createTextNode(fish);
+	para.appendChild(node);
+	fishtank.appendChild(para);
+})
+
+// const fishp = document.getElementById("fish")
+// const fishp2 = document.getElementById("fish2")
+// const fishp3 = document.getElementById("fish3")
+//
+// fishp.textContent = fish
+// fishp2.textContent = fish2
+// fishp3.textContent = fish3
+
