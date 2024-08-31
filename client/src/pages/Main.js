82cd559ebb0a6f6fdb7b38c7f425793a75826315
JumesP <118614403+JumesP@users.noreply.github.com>
2024-08-31 23:57:22 +0100

    created base layout for Main

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -4,12 +4,17 @@ import FishTank from "../components/molecules/fishTank";
 import ArrayOfCards from "../components/molecules/ArrayOfCards";
 import Fish from "../components/atoms/fish";
 
+import './Main.css'
+
 const Main = () => {
 
 
 
 	return (
-		<div>Hello</div>
+		<div className="main">
+			<div className="left">s</div>
+			<div className="right">s</div>
+		</div>
 	);
 }
 
