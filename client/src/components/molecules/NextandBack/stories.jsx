20e9ad6aed39bca78ab5dd48a2e9503bbdd710c4
JumesP <118614403+JumesP@users.noreply.github.com>
2024-09-05 23:13:05 +0000

    created next and back buttons

---
+++ client/src/components/molecules/NextandBack/stories.jsx
@@ -0,0 +1,12 @@
+import NextandBack from './index';
+
+export default {
+    title: "Molecules/NextandBack",
+    component: NextandBack,
+}
+
+const Template = (props) => {
+    return <NextandBack {...props} />
+}
+
+export const Original = Template.bind({});
