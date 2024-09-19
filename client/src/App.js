40ce12516a2dfa88a2e3f4c885e3109ebf37d915
JumesP <118614403+JumesP@users.noreply.github.com>
2024-09-19 22:48:26 +0100

    removed unneeded stuffs

--- client/src/App.js
+++ client/src/App.js
@@ -11,25 +11,12 @@ import Catch from "./pages/Catch";
 import Login from "./pages/Login";
 
 import sendCookieDataToBackend from './utils/sendCookieDataToBackend';
-import ArrayOfCards from "./components/molecules/ArrayOfCards";
-import Fish from "./components/atoms/fish";
-import cod from './images/cod.png';
 
 import './components/styles.scss';
 
 
 function App() {
-
-  // const [backendData, setBackendData] = useState([{}]);
-  //
-  // useEffect(() => {
-  //   fetch('/api/populate')
-  //     .then((res) => res.json())
-  //     .then((data) => setBackendData(data));
-  // }, []);
-
     sendCookieDataToBackend();
-
 
     return (
       <BrowserRouter>
@@ -44,39 +31,7 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
-    )
-
-
-
-  // return (
-  //   <div>
-  //     {console.log(backendData)}
-  //     {/*{(typeof backendData.fish === 'undefined' || typeof backendData.cards === 'undefined') ?*/}
-  //     {(typeof backendData.fish === 'undefined') ?
-  //       <p>Loading...</p> :
-  //       <>
-  //
-  //         <FishTank
-  //             children={backendData.fish.map((fish, number) => (
-  //                 <Fish fishType={fish} path={"/images/" + fish + ".png"}/>
-  //             ))}
-  //         />
-  //
-  //         <h1 className="text-3xl font-bold underline">Fetched Fishes! All Fishes owned:</h1>
-  //         <ul>
-  //           {backendData.fish.map((user, number) => (
-  //             <li key={number}>{user}</li>
-  //           ))}
-  //         </ul>
-  //         {/*{console.log(backendData.cards)} // undefined???*/}
-  //         <ArrayOfCards cards={backendData.reward} />
-  //       </>
-  //     }
-  //   </div>
-  // );
-
-
-
+    );
 }
 
 export default App;
