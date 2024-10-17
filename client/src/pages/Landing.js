6348f5f0d18861f6e56071449a62b09d56e1fe95
JumesP <118614403+JumesP@users.noreply.github.com>
2024-10-17 17:57:37 +0100

    moved fishes to behind title screen

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -4,6 +4,9 @@ import "./css/Landing.scss";
 const Landing = () => {
 	return (
 		<div id="fishTank" className="landing">
+			<Fish fishType="cod" path="/images/cod.png" />
+			<Fish fishType="salmon" path="/images/salmon.png" />
+			<Fish fishType="shark" path="/images/shark.png" />
 			<h1>Welcome to FishingGame</h1>
 			<p>
 				Here you can see all the fish you own and the rewards you have
@@ -11,9 +14,6 @@ const Landing = () => {
 			</p>
 			<p>Click on the Fish Tank to see all the fish you own.</p>
 			<p>Click on the Fish to see the rewards you have earned.</p>
-			<Fish fishType="cod" path="/images/cod.png" />
-			<Fish fishType="salmon" path="/images/salmon.png" />
-			<Fish fishType="shark" path="/images/shark.png" />
 		</div>
 	);
 };
