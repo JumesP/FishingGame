06c1e783cd19f06f72fbcca8201b60b1f661691b
JumesP <118614403+JumesP@users.noreply.github.com>
2024-11-21 16:22:39 +0000

    switched text and fishes

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -4,13 +4,6 @@ import "./css/Landing.scss";
 const Landing = () => {
 	return (
 		<div id="fishTank" className="landing">
-			<Fish fishType="cod" path="/images/cod.png" />
-			<Fish fishType="cod" path="/images/cod.png" />
-			<Fish fishType="salmon" path="/images/salmon.png" />
-			<Fish fishType="salmon" path="/images/salmon.png" />
-			<Fish fishType="goldfish" path="/images/goldfish.png" />
-			<Fish fishType="goldfish" path="/images/goldfish.png" />
-			<Fish fishType="shark" path="/images/shark.png" />
 			<h1>Welcome to FishingGame</h1>
 			<p>
 				Here you can see all the fish you own and the rewards you have
@@ -18,6 +11,14 @@ const Landing = () => {
 			</p>
 			<p>Click on the Fish Tank to see all the fish you own.</p>
 			<p>Click on the Fish to see the rewards you have earned.</p>
+
+			<Fish fishType="cod" path="/images/cod.png" />
+			<Fish fishType="cod" path="/images/cod.png" />
+			<Fish fishType="salmon" path="/images/salmon.png" />
+			<Fish fishType="salmon" path="/images/salmon.png" />
+			<Fish fishType="goldfish" path="/images/goldfish.png" />
+			<Fish fishType="goldfish" path="/images/goldfish.png" />
+			<Fish fishType="shark" path="/images/shark.png" />
 		</div>
 	);
 };
