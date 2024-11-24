99e4bddfaa3a5a92878215f72fa931831af29a8b
JumesP <118614403+JumesP@users.noreply.github.com>
2024-11-24 20:21:55 +0000

    rm FishManagement from Pages/FishManagement
    see last commit!

--- client/src/pages/FishManagement.js
+++ client/src/pages/FishManagement.js
@@ -1,27 +1,20 @@
 import React, { useState, useEffect } from "react";
+import FishManagement from "../components/molecules/FishViewer";
 
-const FishManagement = () => {
-	// const [fish, setFish] = useState([{}]);
-	//
-	// useEffect(() => {
-	// 	fetch("/api/getFish/")
-	// 		.then((res) => res.json())
-	// 		.then((data) => setFish(data));
-	// }, []);
-	//
-	// return (
-	// 	<div>
-	// 		<h1>Fish Management</h1>
-	// 		{fish.map((item) => (
-	// 			<div key={item.id}>
-	// 				<p>{item.name}</p>
-	// 				<p>{item.species}</p>
-	// 				<p>{item.description}</p>
-	// 			</div>
-	// 		))}
-	// 	</div>
-	// );
-	return <p>Welcome to the fish management page!</p>;
+const Inventory = () => {
+	const [fish, setFish] = useState([{}]);
+
+	useEffect(() => {
+		fetch("/api/getFish/")
+			.then((res) => res.json())
+			.then((data) => setFish(data));
+	}, []);
+
+	return (
+		<div>
+			<FishManagement fish={fish} />
+		</div>
+	);
 };
 
-export default FishManagement;
+export default Inventory;
