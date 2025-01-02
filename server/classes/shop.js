d8788bffd4f7bd2611fbfcca4d75c4df75a09c83
JumesP <118614403+JumesP@users.noreply.github.com>
2025-01-02 02:24:31 +0000

    changed shop api data

--- server/classes/shop.js
+++ server/classes/shop.js
@@ -41,93 +41,121 @@ class Shop {
 	}
 
 	getShopBundles() {
-		const currentShop =
-			{
-				ShopDate: "2021-07-07",
-				bundle:
-					[
+		const currentShop = {
+			ShopDate: "2021-07-07",
+			bundle: [
+				{
+					BundleName: "Premium Bundle",
+					BundleCost: 99,
+					BundleType: "LargeBundle",
+					BundleItems: [
 						{
-						BundleName: "Premium Bundle",
-						BundleCost: 99,
-						BundleType: "LargeBundle",
-						BundleItems:
-							[
-								{
-									image: "images/rod.png",
-									header: "Common Rod",
-									details: {
-										enchants: "None",
-										rarity: "Common",
-										durability: "100%",
-										type: "Tool",
-									},
-								},
-								{
-									image: "images/rod.png",
-									header: "Rare Rod",
-									details: {
-										//enchants: 'Focus I',
-										enchants: "🥽",
-										rarity: "Rare",
-										durability: "80%",
-										type: "rod",
-									}
-								},
-								{
-									image: "images/rod.png",
-									header: "Legendary Rod",
-									details: {
-										// enchants: 'Unbreaking III, Mending',
-										enchants: "🏋️🏋️🏋️ + ⛑️",
-										rarity: "Legendary",
-										durability: "95%",
-										type: "rod",
-									}
-								}
-							]
+							image: "images/rod.png",
+							header: "Common Rod",
+							details: {
+								enchants: "None",
+								rarity: "Common",
+								durability: "100%",
+								type: "Tool",
+							},
 						},
 						{
-							BundleName: "Other Premium Bundle",
-							BundleCost: 199,
-							BundleType: "LargeBundle",
-							BundleItems:
-								[
-									{
-										image: "images/rod.png",
-										header: "Common Rod",
-										details: {
-											enchants: "None",
-											rarity: "Common",
-											durability: "100%",
-											type: "Tool",
-										},
-									},
-									{
-										image: "images/rod.png",
-										header: "Rare Rod",
-										details: {
-											//enchants: 'Focus I',
-											enchants: "🥽",
-											rarity: "Rare",
-											durability: "101%",
-											type: "rod",
-										}
-									},
-									{
-										image: "images/rod.png",
-										header: "Legendary Rod",
-										details: {
-											// enchants: 'Unbreaking III, Mending',
-											enchants: "🏋️🏋️🏋️ + ⛑️",
-											rarity: "Legendary",
-											durability: "100%",
-											type: "rod",
-										}
-									}
-								]
-						}
-					]
-			}
+							image: "images/rod.png",
+							header: "Rare Rod",
+							details: {
+								//enchants: 'Focus I',
+								enchants: "🥽",
+								rarity: "Rare",
+								durability: "80%",
+								type: "rod",
+							},
+						},
+						{
+							image: "images/rod.png",
+							header: "Legendary Rod",
+							details: {
+								// enchants: 'Unbreaking III, Mending',
+								enchants: "🏋️🏋️🏋️ + ⛑️",
+								rarity: "Legendary",
+								durability: "95%",
+								type: "rod",
+							},
+						},
+						{
+							image: "images/rod.png",
+							header: "Common Rod",
+							details: {
+								enchants: "None",
+								rarity: "Common",
+								durability: "100%",
+								type: "Tool",
+							},
+						},
+						{
+							image: "images/rod.png",
+							header: "Rare Rod",
+							details: {
+								//enchants: 'Focus I',
+								enchants: "🥽",
+								rarity: "Rare",
+								durability: "80%",
+								type: "rod",
+							},
+						},
+						{
+							image: "images/rod.png",
+							header: "Legendary Rod",
+							details: {
+								// enchants: 'Unbreaking III, Mending',
+								enchants: "🏋️🏋️🏋️ + ⛑️",
+								rarity: "Legendary",
+								durability: "95%",
+								type: "rod",
+							},
+						},
+					],
+				},
+				{
+					BundleName: "Other Premium Bundle",
+					BundleCost: 199,
+					BundleType: "SmallBundle",
+					BundleItems: [
+						{
+							image: "images/rod.png",
+							header: "Common Rod",
+							details: {
+								enchants: "None",
+								rarity: "Common",
+								durability: "100%",
+								type: "Tool",
+							},
+						},
+						{
+							image: "images/rod.png",
+							header: "Rare Rod",
+							details: {
+								//enchants: 'Focus I',
+								enchants: "🥽",
+								rarity: "Rare",
+								durability: "101%",
+								type: "rod",
+							},
+						},
+						{
+							image: "images/rod.png",
+							header: "Legendary Rod",
+							details: {
+								// enchants: 'Unbreaking III, Mending',
+								enchants: "🏋️🏋️🏋️ + ⛑️",
+								rarity: "Legendary",
+								durability: "100%",
+								type: "rod",
+							},
+						},
+					],
+				},
+			],
+		};
 
 		return JSON.stringify(currentShop, null, 2);
 	}
