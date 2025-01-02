28136416a8e60b4f5646ddbc2b08a7a36a417957
JumesP <118614403+JumesP@users.noreply.github.com>
2025-01-02 02:26:44 +0000

    new storybook sample data

--- client/src/components/atoms/shopBundle/stories.jsx
+++ client/src/components/atoms/shopBundle/stories.jsx
@@ -8,6 +8,49 @@ export default {
 
 const Template = (args) => <ShopBundle {...args} />;
 
+export const SmallBundle = Template.bind({});
+SmallBundle.args = {
+	bundle: {
+		BundleName: "Premiumer Bundle",
+		BundleCost: 99,
+		BundleType: "SmallBundle",
+		BundleItems: [
+			{
+				image: "images/rod.png",
+				header: "Common Rod",
+				details: {
+					enchants: "None",
+					rarity: "Common",
+					durability: "100%",
+					type: "Tool",
+				},
+			},
+			{
+				image: "images/rod.png",
+				header: "Rare Rod",
+				details: {
+					//enchants: 'Focus I',
+					enchants: "🥽",
+					rarity: "Rare",
+					durability: "80%",
+					type: "rod",
+				},
+			},
+			{
+				image: "images/rod.png",
+				header: "Legendary Rod",
+				details: {
+					// enchants: 'Unbreaking III, Mending',
+					enchants: "🏋️🏋️🏋️ + ⛑️",
+					rarity: "Legendary",
+					durability: "95%",
+					type: "rod",
+				},
+			},
+		],
+	},
+};
+
 export const LargeBundle = Template.bind({});
 LargeBundle.args = {
 	bundle: {
@@ -77,6 +120,27 @@ LargeBundle.args = {
 					rarity: "Legendary",
 					durability: "95%",
 					type: "rod",
+				},
+			},
+		],
+	},
+};
+
+export const FeaturedItem = Template.bind({});
+FeaturedItem.args = {
+	bundle: {
+		BundleName: "Featured Best Bundle",
+		BundleCost: 999,
+		BundleType: "FeaturedItem",
+		BundleItems: [
+			{
+				image: "images/rod.png",
+				header: "Common Rod",
+				details: {
+					enchants: "None",
+					rarity: "Common",
+					durability: "100%",
+					type: "Tool",
 				},
 			},
 		],
